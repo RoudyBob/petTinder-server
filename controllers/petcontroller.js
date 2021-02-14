9c0421d7dc3a714d5f1d64d4c97cc4f130b40537
susanwulfhawes <62613493+susanwulfhawes@users.noreply.github.com>
2021-02-14 16:07:28 -0500

    git making me commit so I can pull from develop

--- controllers/petcontroller.js
+++ controllers/petcontroller.js
@@ -2,3 +2,10 @@ let express = require('express');
 const Pet = require('../db').import('../models/pet');
 let router = express.Router();
 module.exports = router;
+const validateSession = require ('../middleware/validate-session');
+
+router.get('/', validateSession, (req, res) => {
+    Pet.findAll()
+    .then(pet => res.status(200).json(pet))
+    .catch(err => res.status(500).json({ error: err });
+});
