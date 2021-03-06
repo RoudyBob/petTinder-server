eacd1b2b21c056dd63336b5b9bbb5bce3b17682e
Bob Roudebush <bob@roudybob.net>
2021-03-05 21:36:21 -0500

    fixed SSL

--- db.js
+++ db.js
@@ -9,9 +9,11 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
-        ssl: true,
-        rejectUnauthorized: false,
-    }
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
 
 sequelize.authenticate().then(
