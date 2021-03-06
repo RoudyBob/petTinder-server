619e9999a3afc6323f0736089ee8d8141a96b477
Bob Roudebush <bob@roudybob.net>
2021-03-05 19:13:40 -0500

    initial commit

--- db.js
+++ db.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-})
+    dialectOptions: {
+        ssl: true
+    }
+});
 
 sequelize.authenticate().then(
     function() {
